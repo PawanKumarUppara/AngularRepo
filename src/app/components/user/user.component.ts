c4b7dbdb23163563309b50b4b36a9e42d27032fb
PawanKumarUppara <pawan.uppara@gmil.com>
2018-05-05 23:14:20 -0700

    create own components

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
- import {Component} from '@angular/core';
+ import {Component,OnInit} from '@angular/core';
+import { User } from '../models/User';
 // import { Component } from '@angular/core';
 @Component({
     selector: 'app-user',
@@ -8,56 +9,30 @@ templateUrl: './user.component.html',
 styleUrls: ['./user.component.css']
 })
 // properties
-export class UserComponent {
-firstName: string;
-lastName: string;
-age: number;
-mixedArray: any;
-numberArray: number[];
-address;
+export class UserComponent implements OnInit {
 
-unusablt:  void;
-u: undefined;
-n: null;
-constructor(){
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    this.age = 30;
-    this.numberArray = [1, 2 , 3];
-    this.mixedArray = ['a' , 1];
-    this.unusablt = undefined;
-    this.n = null;
-    this.address = {
-        street: '50 main street',
-        city: 'Boston',
-        State: 'MA'
-        };
+user: User;
 
-    console.log('hi user');
-    this.sayHello();
+constructor() {
+    
 }
 
-
-addNumbers(num1: number,num2: number): number {
-return num1 + num2;
-}
-
-sayHello(){
+ngOnInit() {
 
 
-    console.log('hi' + ' ' + this.firstName);
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
-    console.log(this.addNumbers(2, 3 ));
+    this.user = {
+        firstName: 'John',
+        lastName: 'Doe' ,
+        age: 30 ,
+        address: {
+            street: '50 main street',
+            city: 'Boston',
+            state: 'MA'
+        }
+    };
+    console.log('on init.....');
+  }
+
 
 }
 
-hasBirthday() {
-    this.age += 1;
-}
-
-showAge() {
-    return this.age;
-}
-}
